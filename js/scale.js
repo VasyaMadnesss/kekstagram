20bca8e6e0c6e990cdd0ba448aca51b2ec416b0e
Дубровин Василий <thebestmane@outlook.com>
2024-08-15 13:38:21 +0300

    scale.js refactor

--- js/scale.js
+++ js/scale.js
@@ -1,15 +1,15 @@
-const scaleMinusButton = document.querySelector('.scale__control--smaller');
-const scalePlusButton = document.querySelector('.scale__control--bigger');
-const scaleValueField = document.querySelector('.scale__control--value');
-const scaledImage = document.querySelector('.img-upload__preview > img');
-
 const STEP = 25;
 const MAX_VALUE = 100;
 const MIN_VALUE = 25;
 const DEFAULT_VALUE = 100;
 
+const scaleMinusButton = document.querySelector('.scale__control--smaller');
+const scalePlusButton = document.querySelector('.scale__control--bigger');
+const scaleValueField = document.querySelector('.scale__control--value');
+const scaledImage = document.querySelector('.img-upload__preview > img');
+
 const scaleDown = () => {
-  const currentValue = parseInt(scaleValueField.value, 10);
+  const currentValue = Number(scaleValueField.value);
   if (currentValue > MIN_VALUE) {
     scaleValueField.value = `${currentValue - STEP}%`;
     scaledImage.style.transform = `scale(${(currentValue - STEP) / MAX_VALUE})`;
@@ -17,23 +17,31 @@ const scaleDown = () => {
 };
 
 const scaleUp = () => {
-  const currentValue = parseInt(scaleValueField.value, 10);
+  const currentValue = Number(scaleValueField.value);
   if (currentValue < MAX_VALUE) {
     scaleValueField.value = `${currentValue + STEP}%`;
     scaledImage.style.transform = `scale(${(currentValue + STEP) / MAX_VALUE})`;
   }
 };
 
-const initializeScale = () => {
-  scaleValueField.value = `${DEFAULT_VALUE}%`;
-  scaledImage.style.transform = 'scale(1)';
+const addEventListeners = () => {
   scaleMinusButton.addEventListener('click', scaleDown);
   scalePlusButton.addEventListener('click', scaleUp);
 };
 
-const destroyScale = () => {
+const removeEventListeners = () => {
   scaleMinusButton.removeEventListener('click', scaleDown);
   scalePlusButton.removeEventListener('click', scaleUp);
+};
+
+const initializeScale = () => {
+  scaleValueField.value = `${DEFAULT_VALUE}%`;
+  scaledImage.style.transform = 'scale(1)';
+  addEventListeners();
+};
+
+const destroyScale = () => {
+  removeEventListeners();
 };
 
 export {initializeScale, destroyScale};
